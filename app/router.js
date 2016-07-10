c68712d811ef8d8027761e50d5232da2934b75fc
Katrina Nova <katrina.a.nova@gmail.com>
2016-07-10 16:25:31 -0400

    set routes

--- app/router.js
+++ app/router.js
@@ -3,5 +3,10 @@ import { Router, Route, hashHistory } from 'react-router';
 
 export default (
   <Router history={hashHistory}>
+    <Route component={Layout}>
+      <Route path="/" component={ProductListContainer}/>
+      <Route path="/selected-products" component={SelectedListContainer}/>
+      <Route path="/product_form" component={AddProductFormContainer}/>
+    </Route>
   </Router>
 );
